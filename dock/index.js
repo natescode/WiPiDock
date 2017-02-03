45c06351762fecf8dc20634b60db3b4ac5b5420e
natescode <nate@natescode.com>
2017-02-02 22:51:27 -0600

    Updated gitignore to ignore generated .conf files for hostapd tool. Removed 'docs' folder, no longer using that for the github pages. Instead of docs folder, all project directories have a 'readme.md' to discribe the setup and use for each project type

---
+++ dock/index.js
@@ -0,0 +1,47 @@
+// app/index.js
+// const calc = require('./calc');
+//
+// const numbersToAdd = [
+//     3,
+//     4,
+//     10,
+//     2
+// ];
+//
+// const result = calc.sum(numbersToAdd);
+// console.log(`The result is: ${result}`);
+// console.log('hello, world!');
+//
+// // Move the mouse across the screen as a sine wave.
+// var robot = require("robotjs");
+//
+// // Speed up the mouse.
+// robot.setMouseDelay(2);
+//
+// var twoPI = Math.PI * 2.0;
+// var screenSize = robot.getScreenSize();
+// var height = (screenSize.height / 2) - 10;
+// var width = screenSize.width;
+//
+// for (var x = 0; x < width; x++)
+// {
+//     y = height * Math.sin((twoPI * x) / width) + height;
+//     robot.moveMouse(x, y);
+// }
+
+var hostapd = require('wireless-tools/hostapd');
+
+var options = {
+    channel: 6,
+    driver: 'rt2800usb',
+    hw_mode: 'g',
+    interface: 'wlx14358b0f2232',
+    ssid: 'RaspberryPi',
+    wpa: 2,
+    wpa_passphrase: 'raspberry'
+};
+
+hostapd.enable(options, function(err) {
+    // the access point was created
+    console.log('Wireless Access Point Created Successfully');
+});
